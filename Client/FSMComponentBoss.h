6045ba9d5fa045bb7c0c1a47aab047105e24bc20
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2026-02-13 07:48:18 +0900

    Update FSMComponentBoss.h

--- Client/FSMComponentBoss.h
+++ Client/FSMComponentBoss.h
@@ -43,7 +43,7 @@ private:
 	bool m_attack_has_hit = false;
 	bool m_jump_has_hit = false;
 
-	const float kAttackRange = 16.0f;
+	const float kAttackRange = 12.0f;
 	const float kAttackAnticipation = 32.5f / 24.0f;
 	const float kAttackTotalTime = 59.0f / 24.0f;
 	const float kJumpAttackAnticipation = 60.0f / 24.0f;
