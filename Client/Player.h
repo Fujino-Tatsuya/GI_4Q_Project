a6cd9ef8342d3a93785606860de456c4e041fc12
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2026-02-12 20:34:05 +0900

    End game on boss fade; set player HP to 10
    When the boss death fade completes, call GameManager::SetSuccess(false) and switch to the EndingScene (added GameManager include). Also adjust Player hit points: initial m_playerHitPoint changed from 3 to 10 and m_maxPlayerHitPoint changed from 1000 to 10 to align starting and max HP.

--- Client/Player.h
+++ Client/Player.h
@@ -35,11 +35,9 @@ class Player : public GameObjectBase
 	friend class GameManager;
 
 	std::array<std::pair<com_ptr<ID3D11ShaderResourceView>, DirectX::XMFLOAT2>, 3> m_playerHitPointExpressions = {};
-	std::pair<com_ptr<ID3D11ShaderResourceView>, DirectX::XMFLOAT2> m_playerProfileBGTextureAndOffset = {};
 	std::pair<com_ptr<ID3D11ShaderResourceView>, DirectX::XMFLOAT2> m_playerHitPointTextureAndOffset = {};
-	std::pair<com_ptr<ID3D11ShaderResourceView>, DirectX::XMFLOAT2> m_playerHitPointDecoTextureAndOffset = {};
-	int m_playerHitPoint = 1000;
-	const int m_maxPlayerHitPoint = 1000;
+	int m_playerHitPoint = 10;
+	const int m_maxPlayerHitPoint = 10;
 	const float m_invincibilityDuration = 1.0f;
 	float m_invincibilityTimer = 0.0f;
 	float m_redVignetteIntensity = 0.0f;
